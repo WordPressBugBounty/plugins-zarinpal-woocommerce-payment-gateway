ddde7ebdc3d336208a8838edd5f76f27c7e32a9b
Sean Murphy <sean@iamseanmurphy.com>
2024-12-31 10:18:25 +0000

    5.0.14

--- zarinpal-woocommerce-payment-gateway/index.php
+++ zarinpal-woocommerce-payment-gateway/index.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Plugin Name: افزونه پرداخت امن زرین‌پال برای ووکامرس
-Version: 5.0.13
+Version: 5.0.14
 Description: افزونه درگاه پرداخت امن زرین‌پال برای فروشگاه ساز ووکامرس
 Plugin URI: https://zarinpal.com
 Author: Masoud Amini, Armin Zahedi
@@ -18,8 +18,20 @@ License URI: https://www.gnu.org/licenses/gpl-3.0.en.html
 
 include_once("class-wc-gateway-zarinpal.php");
 
-add_action('before_woocommerce_init', function() {
-    if ( class_exists('\Automattic\WooCommerce\Utilities\FeaturesUtil') ) {
+add_action('before_woocommerce_init', function () {
+    if (class_exists('\Automattic\WooCommerce\Utilities\FeaturesUtil')) {
         \Automattic\WooCommerce\Utilities\FeaturesUtil::declare_compatibility('custom_order_tables', __FILE__, true);
+        \Automattic\WooCommerce\Utilities\FeaturesUtil::declare_compatibility('cart_checkout_blocks', __FILE__, true);
     }
 });
+
+add_action('woocommerce_blocks_loaded', 'zarinpal_gateway_block_support');
+function zarinpal_gateway_block_support() {
+    require_once __DIR__ . '/includes/class-wc-zarinpal-gateway-blocks-support.php';
+    add_action(
+        'woocommerce_blocks_payment_method_type_registration',
+        function (Automattic\WooCommerce\Blocks\Payments\PaymentMethodRegistry $payment_method_registry) {
+            $payment_method_registry->register(new WC_Zarinpal_Gateway_Blocks_Support);
+        }
+    );
+}
